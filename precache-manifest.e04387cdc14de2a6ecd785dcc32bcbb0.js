0811b707b01e0de6f5a70539ea95ee3ff281551f
Marco Tisi <marco.tisi@gmail.com>
2020-05-10 10:59:33 +0100

    Updates

---
+++ precache-manifest.e04387cdc14de2a6ecd785dcc32bcbb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a62beb2316d61ed12854824e582b73",
+    "url": "/marcotisi.dev/index.html"
+  },
+  {
+    "revision": "b6424deae7f57cf94a4e",
+    "url": "/marcotisi.dev/static/css/main.f690d06a.chunk.css"
+  },
+  {
+    "revision": "9191ce48b8fd30d3f207",
+    "url": "/marcotisi.dev/static/js/2.b61b96cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcotisi.dev/static/js/2.b61b96cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6424deae7f57cf94a4e",
+    "url": "/marcotisi.dev/static/js/main.d17c5302.chunk.js"
+  },
+  {
+    "revision": "71965aad7d1ab0c542f9",
+    "url": "/marcotisi.dev/static/js/runtime-main.8f19289e.js"
+  }
+]);
