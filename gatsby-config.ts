db2d46be72dd4ebcaa10016fef79846eb0b6ac01
Marco Tisi <marco.tisi@gmail.com>
2021-02-25 22:37:52 +0100

    Change description

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 export const siteMetadata = {
   title: "Marco Tisi",
   description:
-    "Remote Full-Stack Engineer with experience in building e-commerce platforms, CMS and web applications. Skilled in PHP (Laravel, Symfony), JavaScript (Typescript, Node.js, React, VueJs) and DevOps (AWS, Docker, Terraform). Experienced in Object Oriented programming and Functional Programming. Always enjoying learning about new technologies, frameworks and languages. Passionate about photography, travels and surf.",
+    "Remote Full-Stack Engineer with experience in building eCommerce platforms, CMSs and web applications. Skilled in JavaScript (Typescript, Node.js, React, Gatsby), PHP (Laravel, Symfony) and DevOps (AWS, Docker, Terraform). Experienced in Object Oriented Programming and Functional Programming. Always enjoying learning about new technologies, frameworks and languages.",
   siteUrl: "https://marcotisi.dev",
 };
 
